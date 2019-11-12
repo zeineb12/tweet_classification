84cffc9d96efd428d8807361c44bcaf4b6abf78e
Lilia Ellouz <lilia.ellouz@epfl.ch>
2019-11-12 17:33:39 +0100

    create inital pickle

--- pickle_vocab.py
+++ pickle_vocab.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import pickle
 
-
 def main():
+    
     vocab = dict()
     with open('vocab_cut.txt') as f:
         for idx, line in enumerate(f):
             vocab[line.strip()] = idx
-
+            
     with open('vocab.pkl', 'wb') as f:
         pickle.dump(vocab, f, pickle.HIGHEST_PROTOCOL)
 
---
+++ test_script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print('outside main')
+def main():
+    print('hello world')
+
+if __name__ == '__main__':
+    print('oh')
+    main()
