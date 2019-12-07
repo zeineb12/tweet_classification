6d6ff81d0ad773e9e36b333240bdfdb2022c174f
santille <sarantille@gmail.com>
2019-12-07 17:54:29 +0100

    output_dummy.csv
    add prediction

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import csv
+import numpy as np
 
 def open_by_tweets(path,sep="<user>"):
     """
@@ -6,3 +8,17 @@ def open_by_tweets(path,sep="<user>"):
     with open(path,"r") as file:
         train_pos = file.read().replace('\n','').split(sep)
     return train_pos
+
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w', newline='') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
