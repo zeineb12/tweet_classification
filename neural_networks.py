d5a35920c95abd30192c3ffd89252dc28f6a59af
santille <sarantille@gmail.com>
2019-12-18 21:44:55 +0100

    add comments + change name func

--- neural_networks.py
+++ neural_networks.py
@@ -27,17 +27,9 @@ validation_split = 0.05
 verbose = 1
 
 
-def build_simple_nn(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
+def simple_nn(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
      """
     Create the model for a Simple Neural Net
-    INPUT:
-        X_train : list         -
-        y_train : list         -
-        X_test : list          -
-        y_test : list          -
-        vocab_size :           -
-        embedding_matrix :     -
-        maxlen :               -    
     OUTPUT:
         Returns the model trained 
     """
@@ -52,154 +44,120 @@ def build_simple_nn(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,ma
     #Fit model
     model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
     
+    #Evaluate model
     score = model.evaluate(X_test, y_test, verbose)
     print("Test Score:", score[0])
     print("Test Accuracy:", score[1])
     return model
 
-def build_rnn_lstm(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
+def rnn_lstm(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
     """
-    Create the model for a long-short term memory network
-    INPUT:
-        X_train : list
-        y_train : list
-        X_test : list
-        y_test : list
-        vocab_size :
-        embedding_matrix :
-        maxlen :       
-    OUTPUT:
-        Returns the model trained and the history of the training
+    Create the recurrent neural network model for a long-short term memory network
     """
-    model = Sequential()
-    model.add(
-        Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen,
-                  trainable=False,
-                  mask_zero=True))
-    model.add(Masking(mask_value=0.0)) #need masking layer to not train on padding
-    model.add(LSTM(64, return_sequences=False, 
-                   dropout=0.1, recurrent_dropout=0.1))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
 
-    # Compile the model
-    model.compile(
-        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    print(model.summary())
-    model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
+    #Create model
+     model = Sequential()
+     model.add(Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen,trainable=False, mask_zero=True))
+     model.add(Masking(mask_value=0.0)) #need masking layer to not train on padding
+     model.add(LSTM(64, return_sequences=False, dropout=0.1, recurrent_dropout=0.1))
+     model.add(Dense(64, activation='relu'))
+     model.add(Dropout(0.5))
+     model.add(Dense(1))
+     model.add(Activation('sigmoid'))
 
-    score = model.evaluate(X_test, y_test, verbose)
-    print("Test Score:", score[0])
-    print("Test Accuracy:", score[1])
-    return model
+     # Compile the model
+     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+     print(model.summary())
 
-def build_rnn_bi_lstm(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
-    """
-    Create the model for a Long-Short Term Memory network
-    INPUT:
-        X_train : list
-        y_train : list
-        X_test : list
-        y_test : list
-        vocab_size :
-        embedding_matrix :
-        maxlen :       
-    OUTPUT:
-        Returns the model trained and the history of the training
-    """
-    model = Sequential()
-    model.add(Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen , trainable=False))
-    model.add(Bidirectional(LSTM(64)))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
-    
-    model.compile(
-        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    print(model.summary())
-    
-    model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
+     #Fit model
+     model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
 
-    score = model.evaluate(X_test, y_test, verbose)
-    print("Test Score:", score[0])
-    print("Test Accuracy:", score[1])
-    return model
+     #Evaluate model
+     score = model.evaluate(X_test, y_test, verbose)
+     print("Test Score:", score[0])
+     print("Test Accuracy:", score[1])
+     return model
+
+def rnn_bi_lstm(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
+     """
+     Create the model for a Bi-Directional Long-Short Term Memory network
+     """
+     #Create model
+     model = Sequential()
+     model.add(Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen , trainable=False))
+     model.add(Bidirectional(LSTM(64)))
+     model.add(Dropout(0.5))
+     model.add(Dense(1, activation='sigmoid'))
+     #compile model
+     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+     print(model.summary())
+     #Fit model
+     model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
+     #Evaluate model
+     score = model.evaluate(X_test, y_test, verbose)
+     print("Test Score:", score[0])
+     print("Test Accuracy:", score[1])
+     return model
 
 
 
 
-def build_rnn_gru(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
-    """
-    Create the model for a Convolutional Neural Network with Gated Recurrent Unit
-    INPUT:
-        X_train : list
-        y_train : list
-        X_test : list
-        y_test : list
-        vocab_size :
-        embedding_matrix :
-        maxlen :       
-    OUTPUT:
-        Returns the model trained and the history of the training
-    """
-    model = Sequential()
-    embedding_layer = Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen , trainable=False)
-    model.add(embedding_layer)
-    model.add(GRU(128, return_sequences=True))
-    model.add(Dropout(0.3))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1,activation='sigmoid'))
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    print(model.summary())
-    
-    model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
+def rnn_gru(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
+     """
+     Create the model for a Convolutional Neural Network with Gated Recurrent Unit
+     """
+     #Create model
+     model = Sequential()
+     embedding_layer = Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen , trainable=False)
+     model.add(embedding_layer)
+     model.add(GRU(128, return_sequences=True))
+     model.add(Dropout(0.3))
+     model.add(Flatten())
+     model.add(Dense(128, activation='relu'))
+     model.add(Dropout(0.5))
+     model.add(Dense(1,activation='sigmoid'))
+     #Compile model
+     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+     print(model.summary())
+     #Fit model
+     model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
+     #Evaluate model
+     score = model.evaluate(X_test, y_test, verbose)
+     print("Test Score:", score[0])
+     print("Test Accuracy:", score[1])
+     return model
 
-    score = model.evaluate(X_test, y_test, verbose)
-    print("Test Score:", score[0])
-    print("Test Accuracy:", score[1])
-    return model
+def cnn(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
+     """
+     Create the model for a Convolutional Neural Network with 4 convolutions
+     """
+     #Create model
+     filters = 600
+     kernel_size = 3
 
-def build_cnn(X_train,y_train,X_test,y_test,vocab_size,embedding_matrix,maxlen):
-    """
-    Create the model for a Convolutional Neural Network
-    INPUT:
-        X_train : list
-        y_train : list
-        X_test : list
-        y_test : list
-        vocab_size :
-        embedding_matrix :
-        maxlen :       
-    OUTPUT:
-        Returns the model trained and the history of the training
-    """
-    filters = 600
-    kernel_size = 3
-
-    model = Sequential()
-    model.add(Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen))
-    model.add(Dropout(0.25))
-    model.add(Conv1D(filters, kernel_size, padding='valid', activation='relu', strides=1))
-    model.add(Conv1D(300, kernel_size, padding='valid', activation='relu', strides=1))
-    model.add(Conv1D(150, kernel_size, padding='valid', activation='relu', strides=1))
-    model.add(Conv1D(75, kernel_size, padding='valid', activation='relu', strides=1))
-    model.add(Flatten())
-    model.add(Dense(600))
-    model.add(Dropout(0.5))
-    model.add(Activation('relu'))
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
-    model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
-
-    score = model.evaluate(X_test, y_test, verbose)
-    print("Test Score:", score[0])
-    print("Test Accuracy:", score[1])
-    return model
+     model = Sequential()
+     model.add(Embedding(vocab_size, dim, weights=[embedding_matrix], input_length=maxlen))
+     model.add(Dropout(0.25))
+     model.add(Conv1D(filters, kernel_size, padding='valid', activation='relu', strides=1))
+     model.add(Conv1D(300, kernel_size, padding='valid', activation='relu', strides=1))
+     model.add(Conv1D(150, kernel_size, padding='valid', activation='relu', strides=1))
+     model.add(Conv1D(75, kernel_size, padding='valid', activation='relu', strides=1))
+     model.add(Flatten())
+     model.add(Dense(600))
+     model.add(Dropout(0.5))
+     model.add(Activation('relu'))
+     model.add(Dense(1))
+     model.add(Activation('sigmoid'))
+     #Compile model
+     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+     print(model.summary())
+     #Fit model
+     model.fit(X_train, y_train, batch_size, epochs, verbose, validation_split)
+     #Evaluate model
+     score = model.evaluate(X_test, y_test, verbose)
+     print("Test Score:", score[0])
+     print("Test Accuracy:", score[1])
+     return model
 
 
 
