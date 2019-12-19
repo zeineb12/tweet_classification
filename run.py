c94610409d5a059f117a844399435bbbe5667c1b
santille <sarantille@gmail.com>
2019-12-19 15:53:52 +0100

    correct 3 models
    still need to specifiy the thresholds

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from neural_networks import *
 from embeddings import *
 from ensemble import *
 
-MAXLEN = 40
+MAXLEN = 140
 NUM_WORD = 120000
 
 #load and clean the dataset
@@ -18,7 +18,7 @@ create_and_save_embedding_vect(train_set)
 #split training set
 X_train, X_test, y_train, y_test = train_test_split(train_set['tweet'], train_set['label'], test_size=0.05, random_state=42)
 
-X_train, X_test, y_train, y_test = X_train[0:100], X_test[0:100], y_train[0:100], y_test[0:100]
+X_train, X_test, y_train, y_test = X_train, X_test, y_train, y_test
 #prepare input for neural nets
 X_train,X_test, vocab_size, tokenizer = tok_and_pad_for_nn(X_train, X_test, MAXLEN, NUM_WORD)
 
@@ -27,7 +27,7 @@ embedding_matrix = create_embedding_matrix(vocab_size, tokenizer)
 
 
 #Train and run first model + prediction on unknown set
-model_1 = rnn_bi_lstm(X_train, y_train,X_test,y_test,vocab_size,embedding_matrix, MAXLEN)
+model_1 = cnn(X_train, y_train,X_test,y_test,vocab_size,embedding_matrix, MAXLEN,first_dropout = 0.40)
 preds_1 = compute_predictions_nn(to_predict = unknown, threshold = 0.5, model = model_1, tokenizer = tokenizer,maxlen=MAXLEN)
 create_csv_submission(preds_1, "./ensemble/model1_pred.csv")
 
@@ -37,7 +37,7 @@ preds_2 = compute_predictions_nn(to_predict = unknown, threshold = 0.5, model = 
 create_csv_submission(preds_2, "./ensemble/model2_pred.csv")
 
 #Train and run 3rd model + prediction on unknown set
-model_3 = rnn_lstm(X_train, y_train,X_test,y_test,vocab_size,embedding_matrix, MAXLEN)
+model_3 = rnn_bilstm(X_train, y_train,X_test,y_test,vocab_size,embedding_matrix, MAXLEN)
 preds_3 = compute_predictions_nn(to_predict = unknown, threshold = 0.5, model = model_3, tokenizer = tokenizer,maxlen=MAXLEN)
 create_csv_submission(preds_3, "./ensemble/model3_pred.csv")
 
